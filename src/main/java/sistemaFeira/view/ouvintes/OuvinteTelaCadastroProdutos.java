ba0534e399b5d5b8f8a5827214a562be067e46d2
Taylor Hudson <taylorhudsonsantos@gmail.com>
2022-08-19 13:43:56 -0300

    Tratando erro no ouvinte da tela de cadastro.

--- src/main/java/sistemaFeira/view/ouvintes/OuvinteTelaCadastroProdutos.java
+++ src/main/java/sistemaFeira/view/ouvintes/OuvinteTelaCadastroProdutos.java
@@ -72,29 +72,48 @@ public class OuvinteTelaCadastroProdutos implements MouseListener{
 		}
 		
 		else if(e.getSource() == tela.getLblExculir()) {
-			DefaultTableModel model = (DefaultTableModel) tela.getTbPreco().getModel();
-
-			Object id = tela.getTbPreco().getValueAt(tela.getTbPreco().getSelectedRow(), 0);
-		
-			Produto p = repo.buscarPorId(Integer.parseInt(id.toString()));
-			repo.excluirProduto(p);
+			try {
+				DefaultTableModel model = (DefaultTableModel) tela.getTbPreco().getModel();
+	
+				Object id = tela.getTbPreco().getValueAt(tela.getTbPreco().getSelectedRow(), 0);
 			
-			model.removeRow(tela.getTbPreco().getSelectedRow());
-			
-			limparTexto();
+				Produto p = repo.buscarPorId(Integer.parseInt(id.toString()));
+				repo.excluirProduto(p);
+				
+				model.removeRow(tela.getTbPreco().getSelectedRow());
+				
+				limparTexto();
+			} catch (Exception e2) {
+				JOptionPane.showMessageDialog(null, "Selecione um produto para ser excluido.", "Atenção!", 2, null);
+			}
 		}
 		
 		else if(e.getSource() == tela.getLblAtualizar()) {
-				Object id = tela.getTbPreco().getValueAt(tela.getTbPreco().getSelectedRow(), 0);
-				Object nome = tela.getTxtNomeProduto().getText();
-				Object preco = tela.getTxtPreco().getText();
-				Produto p = new Produto(Integer.parseInt(id.toString()), nome.toString(), preco.toString());
-		
-				repo.atualizarProduto(p);
-				limparTabela();
-				tela.listarProdutosTabela();	
-				
-				limparTexto();
+			try {
+					String nomeProduto = tela.getTxtNomeProduto().getText();
+					String precoProduto = tela.getTxtPreco().getText();
+					
+					if(!nomeProduto.equals("NOME DO PRODUTO") && !nomeProduto.isBlank()
+							&& !precoProduto.equals("PREÇO DO PRODUTO") && !precoProduto.isBlank()) {
+						
+						Object id = tela.getTbPreco().getValueAt(tela.getTbPreco().getSelectedRow(), 0);
+						Object nome = tela.getTxtNomeProduto().getText();
+						Object preco = tela.getTxtPreco().getText();
+						Produto p = new Produto(Integer.parseInt(id.toString()), nome.toString(), preco.toString());
+						
+						repo.atualizarProduto(p);
+						limparTabela();
+						tela.listarProdutosTabela();
+						limparTexto();
+						
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Dados inválidos.", "Atenção", 2, null);
+					}
+				} catch (Exception e2) {
+					JOptionPane.showMessageDialog(null, "Selecione um produto.", "Atenção!", 2, null);
+				}
+
 		}
 		else if(e.getSource() == tela.getTbPreco()) {
 			Object nome = tela.getTbPreco().getValueAt(tela.getTbPreco().getSelectedRow(), 1);
